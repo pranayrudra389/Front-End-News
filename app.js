4a9c9533e9561e784972b18ff103705b5557fe34
Pranay Kumar <rudrapranaykumar@gmail.com>
2016-05-16 04:08:00 -0700

    Ended Up

--- app.js
+++ app.js
@@ -2,16 +2,43 @@
 //   DOCUMENT READY
 //-----------------------------------------------
 
-$(document).ready(function(){
-	$('.js-like').on('click', function(event){
-		event.preventDefault();
-		
-		$(this).text('Liked!').closest('.news-item').addClass('is-liked');
-	});
-	
-	$('.js-add-link').on('click', function(event){
-		event.preventDefault();
-		
-		$('.js-form').toggleClass('is-visible');
-	});
+$(document).ready(function() {
+
+  //-----------------------------------------------
+  //   Like
+  //-----------------------------------------------
+
+  $('.js-like').on('click', function(event) {
+    event.preventDefault();
+
+    $(this).text('Liked!')
+     .closest('.news-item')
+     .addClass('is-liked');
+  });
+
+  //-----------------------------------------------
+  //   Add Link
+  //-----------------------------------------------
+
+  $('.js-add-link').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-form').toggleClass('is-visible');
+  });
+
+  //-----------------------------------------------
+  //   Modal
+  //-----------------------------------------------
+
+  $('.js-show-modal').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-modal').addClass('is-visible');
+    $('.js-modal-overlay').addClass('is-visible');
+  });
+
+  $('.js-modal-overlay').on('click', function(event) {
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
+  });
 });
